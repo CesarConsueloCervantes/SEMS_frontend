5ac76729063f24005c352f4e8d0f5ea5e3875e83
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-23 20:31:43 -0600

    General update :zap: : configuraciones inciales terminadas

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,8 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+    <Toast />
+    <ConfirmDialog />
+
+    <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
+import Tooltip from 'primevue/tooltip'
+
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Dialog from 'primevue/dialog'
+import Toast from 'primevue/toast'
+import ConfirmDialog from 'primevue/confirmdialog'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +22,18 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(PrimeVue)
+
+app.use(ToastService)
+app.use(ConfirmationService)
+
+app.directive('tooltip', Tooltip)
+
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.component('Dialog', Dialog)
+app.component('Toast', Toast)
+app.component('ConfirmDialog', ConfirmDialog)
 
 app.mount('#app')
