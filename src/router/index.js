06310be20e7896686e099a2f69897b29b9a2dc43
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-24 18:31:25 -0600

    feat :sparkles: (index.js): se configuraron las rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,78 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '@/views/dashboard/DashboardView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
+import Error404 from '@/views/errors/Error404.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path:'/',
+      redirect: '/dashboard'
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+      meta:{
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: Error404,
     },
   ],
 })
 
 export default router
-
-//// ejemplo de la configuracion
-// import { createRouter, createWebHistory } from 'vue-router'
-
-// import LoginView from '@/views/auth/LoginView.vue'
-// import RegisterView from '@/views/auth/RegisterView.vue'
-// import DashboardView from '@/views/dashboard/DashboardView.vue'
-
-// const router = createRouter({
-
-//     history: createWebHistory(import.meta.env.BASE_URL),
-
-//     routes: [
-
-//         {
-
-//             path: '/',
-
-//             redirect: '/dashboard'
-
-//         },
-
-//         {
-
-//             path: '/login',
-
-//             name: 'login',
-
-//             component: LoginView
-
-//         },
-
-//         {
-
-//             path: '/register',
-
-//             name: 'register',
-
-//             component: RegisterView
-
-//         },
-
-//         {
-
-//             path: '/dashboard',
-
-//             name: 'dashboard',
-
-//             component: DashboardView,
-
-//             meta: {
-
-//                 requiresAuth: true
-
-//             }
-
-//         }
-
-//     ]
-
-// })
-
-// export default router
