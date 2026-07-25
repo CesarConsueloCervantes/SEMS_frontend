52931e9fdbd11c752c491080faea826327854848
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-24 23:23:33 -0600

    feat :sparkles: (index.js): se agrego un redireccionamiento a las rutas

--- src/router/index.js
+++ src/router/index.js
@@ -37,4 +37,15 @@ const router = createRouter({
   ],
 })
 
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem('token')
+
+    if (to.meta.requiresAuth && !token) {
+        next('/login')
+        return
+    }
+
+    next()
+})
+
 export default router
