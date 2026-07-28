7b1ee40e7ad61dd0e97b57dc78cf9d15846b2178
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-28 00:48:09 -0600

    feat :sparkles: (metadataStore.js): se creo el metodo de consulta de los metadatos

--- src/stores/metadataStore.js
+++ src/stores/metadataStore.js
@@ -2,13 +2,14 @@ import { defineStore } from 'pinia'
 import { computed, ref, reactive } from 'vue'
 import { metadataColumns } from '@/constants/metadataColumns'
 import * as metadataService from '@/services/Archives/metadataService'
+import { useAppStore } from '@/stores/appStore'
 
 export const useMetadataStore = defineStore('metadata', () => {
-
-  const metadata = ref([])
+  
+  const appStore = useAppStore()
   const columns = ref(structuredClone(metadataColumns))
 
-  const isloading = ref(false)
+  const loading = ref(false)
   const count = ref(0)
 
   const pagination = reactive({ page: 1, rows: 100, })
@@ -18,4 +19,52 @@ export const useMetadataStore = defineStore('metadata', () => {
       columns.value.filter(column => column.visible)
   )
 
+  async function fetchMetadata() {
+
+    loading.value = true
+
+    try {
+      const query = {
+        page: pagination.page,
+        rows: pagination.rows,
+
+        sortField: sort.field,
+        sortOrder: sort.order,
+
+        columns: JSON.stringify(
+          selectedColumns.value.map(column => column.field)
+        ),
+
+        filters: JSON.stringify(
+          Object.fromEntries(
+            columns.value
+              .filter(column => column.filter)
+              .map(column => [
+                  column.field,
+                  column.filter,
+              ])
+          )
+        ),
+      }
+
+      const response = await metadataService.getMetadataByUser(query)
+
+      appStore.refreshMetadata(
+        Array.isArray(response)
+          ? response  
+          : (response?.data ?? [])  
+      )
+      count.value = response.count ?? 0
+    }
+    catch (error) {
+      console.error(error)
+    }
+    finally {
+      loading.value = false
+    }
+  }
+  
+  return{
+    fetchMetadata,
+  }
 })
